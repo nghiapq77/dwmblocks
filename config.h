22c0bb071a85f4eaa9cb842f15cc4fca8bf49fdf
nghia.pq77 <nghia.pq77@gmail.com>
2020-04-22 23:27:35 +0700

    Temporary status bar

--- config.h
+++ config.h
@@ -1,21 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Command*/	/*Update Interval*/	/*Update Signal*/
-	{"cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"music",	0,	11}, */
-	{"pacpackages",	0,	8},
+	//{"pacpackages",	0,	8},
 	/* {"crypto",	0,	13}, */
 	/* {"torrent",	20,	7}, */
-	{"news",	0,	6},
-	/* {"memory",	10,	14}, */
-	/* {"cpu",		10,	13}, */
-	/* {"moonphase",	18000,	5}, */
-	{"weather",	18000,	5},
-	{"mailbox",	180,	12},
+	//{"news",	0,	6},
+	{"internet",	6,	4},
+	{"memory",	3,	14},
+	{"cpu",		3,	13},
+	{"lang",	0,	10},
 	{"volume",	0,	10},
+	{"bluetooth",	0,	5},
 	{"battery",	5,	3},
 	{"clock",	60,	1},
-	{"internet",	5,	4},
 	{"help-icon",	0,	15},
 };
 
