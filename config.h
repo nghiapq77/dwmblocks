dc3ba37c559e082c1ff0a7d9c9366ab127f24330
Nghia Pham <nghia.pq77@gmail.com>
2021-02-23 21:18:26 +0700

    Change command names

--- config.h
+++ config.h
@@ -1,16 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/ /*Command*/	/*Update Interval*/	/*Update Signal*/
-	{"",     "internet",	5,	                4},
-	{"",     "bluetooth",	10,	                5},
-	{"",     "memory",	2,	                14},
+	{"",     "internet",	5,	                4 },
+	{"",     "bluetooth",	10,	                5 },
+	{"",     "ram",	        2,	                14},
 	{"",     "cpu",		2,	                13},
         {"",	 "mailbox",	180,	                12},
 	{"",     "lang",	0,	                11},
-	{"",     "volume",	0,	                10},
-	{"",     "battery",	30,	                3},
-	{"",     "clock",	60,	                1},
-	{"",     "help-icon",	0,	                15},
+	{"",     "sound",	0,	                10},
+	{"",     "battery",	30,	                3 },
+	{"",     "clock",	60,	                1 },
+	{"",     "tux",	        0,	                15},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
